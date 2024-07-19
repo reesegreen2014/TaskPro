0af0562ea96f9ce5a78022e8bdb3cb4279c7d69b
“Reese <reesegreen2014@me.com>
2024-07-19 13:50:00 -0600

    implementation and display of motivational quote and additional homepage styling

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -7,8 +7,9 @@ function HomePage() {
   return (
     <div className='home-page'>
       <Header />
-      <h1>Welcome to TaskPro!</h1>
+      <h1 className='welcome-message'>Welcome to TaskPro!</h1>
       <MotivationalQuote />
+      <h2 className='your-tasks'><strong>Your Tasks:</strong></h2>
     </div>
   )
 }
