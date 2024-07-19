f3c13994a29ff4ffc5033fccb37d624dde3e05a7
“Reese <reesegreen2014@me.com>
2024-07-19 14:01:31 -0600

    additional styling of homepage, header and api fetch content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
 
-function Header() {
+const Header = () => {
   return (
-    <header className='header'>
-        <h1>TaskPro</h1>
-        <div className='header-buttons'>
-            <button className='header-button'>Add Task</button>
-            <button className='header-button'>About The Project</button>
-        </div>
+    <header className="header">
+      <h1>TaskPro</h1>
+      <div className="header-buttons">
+        <button className="header-button">About the Project</button>
+        <button className="header-button">Add a Task</button>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
