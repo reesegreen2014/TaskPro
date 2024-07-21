c39f96e3edbb81040b692709824247a154bfeb6e
“Reese <reesegreen2014@me.com>
2024-07-21 10:28:16 -0600

    corrects incorrect path for header buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './Header.css'; 
+import './Header.css';
 
 const Header = () => {
   const location = useLocation();
+  const hiddenPaths = ['/about', '/reflections/member1', '/reflections/member2', '/reflections/member3'];
 
   return (
     <header className="header">
       <h1>TaskPro</h1>
-      {location.pathname !== '/about' && (
+      {!hiddenPaths.includes(location.pathname) && (
         <div className="header-buttons">
-          <button className="header-button">Add a Task</button>
-          <Link to="/about" className="header-link header-button">About the Project</Link>
+          <Link to="/add" className="header-button">
+          Add a Task
+        </Link>
+        <button className="header-button">About the Project</button>
         </div>
       )}
     </header>
